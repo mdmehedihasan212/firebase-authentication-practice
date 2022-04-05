4e7464545919ab86dbf40313cd6bb9973641a7ff
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-06 03:53:52 +0600

    add display user information

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app);
+
 function App() {
+  const [user, setUser] = useState({});
+  const googleProvider = new GoogleAuthProvider();
+
+  const GoogleSingIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user)
+      })
+      .catch((error) => {
+        console.error(error);
+      }
+      )
+  };
   return (
     <div className="App">
-      <h1>Start Firebase Authentication</h1>
+      <h1>Firebase Authentication Learning</h1>
+      <>
+        <div className="user-container">
+          <h4>Name: {user.displayName}</h4>
+          <p>E-mail: {user.email}</p>
+        </div>
+        <button onClick={GoogleSingIn}>Google Sing In</button>
+      </>
     </div>
   );
 }
